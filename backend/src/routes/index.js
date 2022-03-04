90494d46a59c5191b16e5255ab1a773bc7d72337
elhs25 <hsortiz05@gmail.com>
2022-03-04 08:01:07 -0500

    🌟 added ability to list messages

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRouter = require('./user');
 const roomRouter = require('./room');
+const messagesRouter = require('./messages');
 
 router.use('/user', userRouter);
 router.use('/room', roomRouter);
+router.use('/message', messagesRouter);
 
 module.exports = router;
--- backend/src/routes/messages/index.js
+++ backend/src/routes/messages/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-const { messagesMiddleware } = require("../../middlewares");
+// const { messagesMiddleware } = require("../../middlewares");
 const { messagesController } = require("../../controllers");
 
 router.get("/:room_id", messagesController.listRoomMessages);
-router.get("/:room_id/send", messagesMiddleware.checkMessageFields, messagesController.sendMessage);
+// router.get("/:room_id/send", messagesMiddleware.checkMessageFields, messagesController.sendMessage);
 
 module.exports = router;
